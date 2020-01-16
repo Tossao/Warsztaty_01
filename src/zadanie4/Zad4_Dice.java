c76fbc8b1481a263973bc804149c6c4fa3b820ea
Tossao <marcinjamroz8@gmail.com>
2020-01-16 21:16:50 +0100

    Zadanie4 - Dice

--- src/zadanie4/Zad4_Dice.java
+++ src/zadanie4/Zad4_Dice.java
@@ -1,189 +1,116 @@
 package src.zadanie4;
 
+import org.jsoup.internal.StringUtil;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
-// NIE SKONCZONE !!!
-// NIE SKONCZONE !!!
-// NIE SKONCZONE !!!
 public class Zad4_Dice {
-
-    static Scanner scanner;
-
     public static void main(String[] args) {
-        scanner = new Scanner(System.in);
-        dice(input());
-        scanner.close();
-    }
 
-    static int dice(String str) {
-        int x = 1;
-        int y;
-        int z = 0;
+        int result = 0;
+        int numberOfRolls = 0;
+        int dice = 0;
+        int mathOperation = 0;
+        char mark = ' ';
+        boolean loop = true;
 
-        StringBuilder builder = new StringBuilder();
-        if (times(str) == 0) {
-            String strDice = str.substring(1);
-            builder.append(strDice);
-            if (mod(strDice) == 10) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "+");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-            } else if (mod(strDice) == 20) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "-");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-            } else {
-                y = diceType(strDice);
+        while (loop) {
+            numberOfRolls = 1;
+            dice = 0;
+            mathOperation = 0;
+            mark = ' ';
+            loop = false;
+            String command = "";
+            System.out.println("Rzuć kością używając schematu xDy+z \n"
+                    + "gdzie: \n"
+                    + "y – rodzaj kostek, których należy użyć (np. D6, D10), \n"
+                    + "x – liczba rzutów kośćmi (jeśli rzucamy raz, ten parametr jest pomijalny), \n"
+                    + "z – (opcjonalnie) liczba, którą należy dodać (lub odjąć) do wyniku rzutów.");
+            System.out.println("Wybierz kości: D3, D4, D6, D8, D10, D12, D20, D100");
+            Scanner scanner = new Scanner(System.in);
+            command = scanner.nextLine();
+            String[] partFirst = command.split("D");
+            if (!command.contains("D")) {
+                System.out.println("Źle wpisana komenda! Brakuje 'D'");
+                loop = true;
+            } else if (partFirst.length != 2) {
+                System.out.println("Źle wpisana komenda!");
+                loop = true;
+            } else if (!partFirst[0].equals("")) {
+                try {
+                    numberOfRolls = Integer.parseInt(partFirst[0]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Przed 'D' musi być liczba !");
+                    loop = true;
+                }
             }
-
-        } else if (times(str) == 1) {
-            char charX = str.charAt(0);
-            x = modifier(Character.toString(charX));
-
-            String strDice = str.substring(2);
-            builder.append(strDice);
-            if (mod(strDice) == 10) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "+");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-            } else if (mod(strDice) == 20) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "-");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-            } else {
-                y = diceType(strDice);
+            if (numberOfRolls < 1) {
+                System.out.println("Przynajmniej raz trzeba rzucic kostka!");
+                loop = true;
             }
-        } else {
-            char x1 = str.charAt(0);
-            char x2 = str.charAt(1);
-            String xPos = Character.toString(x1) + x2;
-            x = modifier(xPos);
+            if (!loop) {
+                if (partFirst[1].contains("+")) {
+                    String[] partSecond = partFirst[1].split("\\+");
+                    mark = '+';
+                    try {
+                        dice = Integer.parseInt(partSecond[0]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Żle wpisany rodzaj kostki! Popraw błąd");
+                        loop = true;
+                    }
+                    try {
+                        mathOperation = Integer.parseInt(partSecond[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Źle wpisana (opcjonalna) liczba! Popraw Błąd");
+                        loop = true;
+                    }
+                } else if (partFirst[1].contains("-")) {
+                    String[] partSecond = partFirst[1].split("-");
+                    mark = '-';
+                    try {
+                        dice = Integer.parseInt(partSecond[0]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Żle wpisany rodzaj kostki! Popraw błąd ");
+                        loop = true;
+                    }
+                    try {
+                        mathOperation = Integer.parseInt(partSecond[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Źle wpisana(opcjonalna) liczba! Popraw błąd.");
+                        loop = true;
+                    }
 
-            String strDice = str.substring(3);
-            builder.append(strDice);
-            if (mod(strDice) == 10) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "+");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-                System.out.println(z);
-            } else if (mod(strDice) == 20) {
-                StringTokenizer tokenizer = new StringTokenizer(strDice, "-");
-                String str1 = tokenizer.nextToken();
-                String str2 = tokenizer.nextToken();
-                y = diceType(str1);
-                z = modifier(str2);
-            } else {
-                y = diceType(strDice);
-            }
-        }
-        System.out.println("Liczba rzutów: " + x + " Rodzaj kostek: D" + y + " Mod: " + z);
-        Random random = new Random();
-        int roll = 0;
-        for (int i = 0; i < x; i++) {
-            int d = random.nextInt(y) + 1;
-            roll = roll + d;
-        }
-        int result;
-        String strDice = builder.toString();
-        if (mod(strDice) == 10) {
-            result = roll + z;
-        } else if (mod(strDice) == 20) {
-            result = roll - z;
-        } else {
-            result = roll;
-        }
-        System.out.println(result);
-        return result;
-    }
+                } else {
+                    try {
+                        dice = Integer.parseInt(partFirst[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Żle wpisany rodzaj kostki! Popraw błąd");
+                        loop = true;
+                    }
+                }
+                if (dice == 3 || dice == 4 || dice == 6 || dice == 8 || dice == 10 || dice == 12 || dice == 20 || dice == 100) {
 
-    static String input() {
-        System.out.println("Wprowadz: xDy +/- z," + "\n" + "gdzie x - ilosc rzutow, y - rodzaj kostki, z - (opcjonalnie) liczba ktora mozna dodac(lub odjac) do wyniku rzutow");
-        StringBuilder temp = new StringBuilder();
-        while (true) {
-            String txt = scanner.nextLine();
-            if (times(txt) < 3) {
-                temp.append(txt);
-                break;
-            }
-            System.out.println("Błąd!!! Wprowadz jeszcze raz: ");
-        }
-        String str = temp.toString();
-        return str;
-    }
-
-    static int mod(String inCode) {
-        int sign = 0;
-        for (int i = 0; i < inCode.length() - 1; i++) {
-            try {
-                char charAtI = inCode.charAt(i);
-                @SuppressWarnings("Nie uzywane")
-                int temp = Integer.parseInt(Character.toString(charAtI));
-                sign++;
-            } catch (NumberFormatException ex) {
-                break;
+                } else {
+                    System.out.println("Zły rodzaj kostki!");
+                    loop = true;
+                }
             }
         }
-        if (sign + 1 < inCode.length()) {
-            if (inCode.charAt(sign) == '+') {
-                return 10;
-            } else if (inCode.charAt(sign) == '-') {
-                return 20;
-            } else {
-                return 30;
-            }
-        } else {
-            return 30;
+        for (int i = 0; i < numberOfRolls; i++) {
+            Random random = new Random();
+            int number = random.nextInt(dice) + 1;
+            result += number;
         }
-    }
-
-    static int times(String inCode) {
-        int dDice = -1;
-        for (int i = 0; i < inCode.length() - 1; i++) {
-            if ((inCode.charAt(i) == 'D') || (inCode.charAt(i) == 'd')) {
-                dDice = i;
-                break;
-            }
+        if (mark == '+') {
+            result += mathOperation;
+        } else if (mark == '-') {
+            result -= mathOperation;
         }
-        return dDice;
-    }
-
-    static int modifier(String str) {
-        int num = 1;
-        try {
-            num = Integer.parseInt(str);
-        } catch (NumberFormatException ex) {
-            System.out.println("Źle wprowadzono dane! Wprowadz jeszcze raz:");
-            dice(input());
-        }
-        return num;
-    }
-
-    static int diceType(String str) {
-        int num = 1;
-        try {
-            num = Integer.parseInt(str);
-            if ((num == 3) || (num == 4) || (num == 6) || (num == 8) || (num == 10) || (num == 12) || (num == 20) || (num == 100)) {
-
-            } else {
-                System.out.println("Zły rodzaj kostki!!! Wprowadz jeszcze raz: ");
-                dice(input());
-            }
-        } catch (NumberFormatException ex) {
-            System.out.println("Błąd");
-            dice(input());
-        }
-        return num;
+        System.out.println("Ilość rzutów: " + numberOfRolls);
+        System.out.println("Typ kostki: D" + dice);
+        System.out.println("Opcjonalne plus/minus: " + mark + mathOperation);
+        System.out.println();
+        System.out.println("Wynik: " + result);
     }
 }
